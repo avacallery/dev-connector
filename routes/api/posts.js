0b1231fa888d06631e0e4d82e4bb9927fa5dca15
avacallery <avacallery@gmail.com>
2020-06-07 15:51:15 -0400

    add like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -110,4 +110,70 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post has been liked by this user
+    //filter through array likes
+    //compare current iteration (using filter) to see if user matches
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    PUT api/posts/like/:id
+// @desc     Unlike a post
+// @access   Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post has been liked by this user
+    //filter through array likes
+    //compare current iteration (using filter) to see if user matches
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not been liked yet' });
+    }
+
+    //remove a like
+    //Get remove index
+    //get correct like to remove using map
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    //splice like out of the array
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
