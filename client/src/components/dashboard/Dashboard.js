2bbc76f1f2bda4ab4316c25b1febe873d42d088a
avacallery <avacallery@gmail.com>
2020-07-18 18:50:54 -0400

    create private route for dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+// Dashboard is where we fetch all of our data using an action and bring it in using Redux state and then pass it down to other components like - experience/education components
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = (props) => {
+  return <div></div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
