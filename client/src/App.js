763595391bd47f8b8bedaa5f2525290fce52695f
avacallery <avacallery@gmail.com>
2020-08-02 18:29:10 -0400

    add experience to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import addExperience from './components/profile-forms/addExperience';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -51,6 +52,11 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={addExperience}
               />
             </Switch>
           </section>
